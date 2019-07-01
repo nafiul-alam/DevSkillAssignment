36457d1f6af4283ed122fffc19f407f60ad606f6
nafiul-alam <nafiulght@gmail.com>
2019-07-01 21:54:23 +0600

    On:-2 Complete

---
+++ Classtest1/Classtest1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Classtest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, r, sum = 0, t;
+
+            Console.WriteLine("Input a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (t = num; t != 0; t = t / 10)
+            {
+                r = t % 10;
+                sum = sum * 10 + r;
+            }
+            Console.WriteLine("The number in reverse order is : {0} \n", sum);
+        }
+    }
+}
